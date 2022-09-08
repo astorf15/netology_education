167839e7d0a4c348e5db89029eed43438243ac5b
astorf15 <astorf@inbox.ru>
2022-09-08 17:18:05 +0300

    Update diplom project

--- homework/diplom-netology/stack/terrform/s3_bucket_yc.tf
+++ homework/diplom-netology/stack/terrform/s3_bucket_yc.tf
@@ -5,8 +5,8 @@ terraform {
     region               = "ru-central1"
     workspace_key_prefix = "environments"
     key                  = "terraform.tfstate"
-    access_key           = "YCAJECc06O1uh3b-TaAacKnGB"
-    secret_key           = "YCM6vp3ONPCLCBNGv2Mo7EhjM4_4Ap5ekJEptxP7"
+    access_key           = "***"
+    secret_key           = "***"
 
     skip_region_validation      = true
     skip_credentials_validation = true
